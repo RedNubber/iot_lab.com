3b72aa3d3e0e62b9201a5fa68049c96e377d500a
RedNubber <157898680+RedNubber@users.noreply.github.com>
2024-02-03 21:03:31 +0700

    Update smarthome.js

--- smarthome.js
+++ smarthome.js
@@ -65,6 +65,90 @@ firebase.database().ref("/TT_IoT/Temp").on("value", function(snapshot){
                                 gauge.querySelector(".gauge-fill4").style.transform = `rotate(${(4096-value) / 9200}turn)`;
                                 gauge.querySelector(".gauge-cover4").textContent = `${4096-value}`;
                             }
+
+
+                            $(document).ready(function(){
+                              var database = firebase.database();
+                              var Led1Status;
+                              var Led2Status;
+                              var Led3Status;
+                              var Led4Status;
+                              database.ref().on("value", function(snap){
+                                Led1Status = snap.val().Led1Status;
+                                Led2Status = snap.val().Led2Status;
+                                Led3Status = snap.val().Led3Status;
+                                Led4Status = snap.val().Led4Status;
+                                if(Led1Status == "1"){
+                                  document.getElementById("unact").style.display = "none";
+                                  document.getElementById("act").style.display = "block";
+                                } else {
+                                  document.getElementById("unact").style.display = "block";
+                                  document.getElementById("act").style.display = "none";
+                                }
+                                if(Led2Status == "1"){
+                                  document.getElementById("unact1").style.display = "none";
+                                  document.getElementById("act1").style.display = "block";
+                                } else {
+                                  document.getElementById("unact1").style.display = "block";
+                                  document.getElementById("act1").style.display = "none";
+                                }
+                                if(Led3Status == "1"){
+                                  document.getElementById("unact2").style.display = "none";
+                                  document.getElementById("act2").style.display = "block";
+                                } else {
+                                  document.getElementById("unact2").style.display = "block";
+                                  document.getElementById("act2").style.display = "none";
+                                }
+                                if(Led4Status == "1"){
+                                  document.getElementById("unact3").style.display = "none";
+                                  document.getElementById("act3").style.display = "block";
+                                } else {
+                                  document.getElementById("unact3").style.display = "block";
+                                  document.getElementById("act3").style.display = "none";
+                                }
+                              });
+                            
+                              $(".toggle-btn").click(function(){
+                                var firebaseRef = firebase.database().ref().child("Led1Status");
+                                if(Led1Status == "1"){
+                                  firebaseRef.set("0");
+                                  Led1Status = "0";
+                                } else {
+                                  firebaseRef.set("1");
+                                  Led1Status = "1";
+                                }
+                              })
+                              $(".toggle-btn1").click(function(){
+                                var firebaseRef = firebase.database().ref().child("Led2Status");
+                                if(Led2Status == "1"){
+                                  firebaseRef.set("0");
+                                  Led2Status = "0";
+                                } else {
+                                  firebaseRef.set("1");
+                                  Led2Status = "1";
+                                }
+                              })
+                              $(".toggle-btn2").click(function(){
+                                var firebaseRef = firebase.database().ref().child("Led3Status");
+                                if(Led3Status == "1"){
+                                  firebaseRef.set("0");
+                                  Led3Status = "0";
+                                } else {
+                                  firebaseRef.set("1");
+                                  Led3Status = "1";
+                                }
+                              })
+                              $(".toggle-btn3").click(function(){
+                                var firebaseRef = firebase.database().ref().child("Led4Status");
+                                if(Led4Status == "1"){
+                                  firebaseRef.set("0");
+                                  Led4Status = "0";
+                                } else {
+                                  firebaseRef.set("1");
+                                  Led4Status = "1";
+                                }
+                              })
+                            });
                             
                             
                   
